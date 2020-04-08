321d0bdf6999c1031a73831e8d20f40ce6f79d40
Cherfez <chervasev.leeuwen@gmail.com>
2020-04-08 15:25:05 +0200

    delete route setup

--- routers/feedback.js
+++ routers/feedback.js
@@ -45,27 +45,22 @@ router.post("/", auth, async (req, res, next) => {
   }
 });
 
-<<<<<<< HEAD
-module.exports = router;
-=======
-router.delete('/:id', auth, async (req, res, next) => {
-  try{
-    if(req.user.id === null){
-      return res.status(400).send({ message: 'Not logged in!' })
+router.delete("/:id", auth, async (req, res, next) => {
+  try {
+    if (req.user.id === null) {
+      return res.status(400).send({ message: "Not logged in!" });
     }
-    const { id } = req.params
-    
-    const comment = await Comment.findByPk(id)
-    if(!comment){
-      return res.status(404).send({message: 'Comment not found'})
+    const { id } = req.params;
+
+    const comment = await Comment.findByPk(id);
+    if (!comment) {
+      return res.status(404).send({ message: "Comment not found" });
     }
     const feedback = await comment.destroy();
     return res.status(201).send({ message: "Feedback deleted", feedback });
-    
-  } catch(e){
-    next(e)
+  } catch (e) {
+    next(e);
   }
-})
+});
 
 module.exports = router;
->>>>>>> 5c7a02ca38ef3c44d69aabdaafeee29771eb8b34
