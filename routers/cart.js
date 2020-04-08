8b761bf15d0cd746285af67483ccc2c173fc6f2f
mariazangelova <mariazangelova@gmail.com>
2020-04-08 16:39:15 +0200

    Delete route for clearing up a cart

--- routers/cart.js
+++ routers/cart.js
@@ -2,70 +2,84 @@ const { Router } = require("express");
 const auth = require("../auth/middleware");
 const Service = require("../models").service;
 const Category = require("../models").category;
-const Comment = require('../models').comment
-const Cart = require('../models').cart
-const Items = require('../models').cartItem
+const Comment = require("../models").comment;
+const Cart = require("../models").cart;
+const Items = require("../models").cartItem;
 
 const router = new Router();
 
 router.get("/", auth, async (req, res, next) => {
   try {
-    if(req.user.id === null){
-      return res.status(400).send({ message: 'Not logged in!' })
+    if (req.user.id === null) {
+      return res.status(400).send({ message: "Not logged in!" });
     }
-    const cart = await Cart.findOne({ 
-    where: {userId: req.user.id},
-    })
-    
-    if(cart === null){
-      return res.status(400).send({ message: 'Cart does not exist!' })
+    const cart = await Cart.findOne({
+      where: { userId: req.user.id },
+    });
+
+    if (cart === null) {
+      return res.status(400).send({ message: "Cart does not exist!" });
     }
-    
+
     const items = await Items.findAll({
-    where: {cartId: cart.id},
-    include: [Service],
-    order: [[Service, "createdAt", "DESC"]]
-    })
-    
-    if(items === null){
-      return res.status(400).send({ message: 'Cart is empty!' })
+      where: { cartId: cart.id },
+      include: [Service],
+      order: [[Service, "createdAt", "DESC"]],
+    });
+
+    if (items === null) {
+      return res.status(400).send({ message: "Cart is empty!" });
     }
-    
-  res.status(200).send({cart, items})
-  
-  } catch(e){
-    next(e)
+
+    res.status(200).send({ cart, items });
+  } catch (e) {
+    next(e);
   }
 });
 
-router.post('/', auth, async (req, res, next) => {
-  try{
-    if(req.user.id === null){
-      return res.status(400).send({ message: 'Not logged in!' })
+router.post("/", auth, async (req, res, next) => {
+  try {
+    if (req.user.id === null) {
+      return res.status(400).send({ message: "Not logged in!" });
     }
-    const cart = await Cart.findOne({ where: {userId: req.user.id }})
-    if(cart === null){
-      return res.status(400).send({ message: 'There is no shoppingcart'})
+    const cart = await Cart.findOne({ where: { userId: req.user.id } });
+    if (cart === null) {
+      return res.status(400).send({ message: "There is no shoppingcart" });
     }
-    const { quantity, serviceId } = req.body
-    
-    if ( quantity <= 0 ) {
+    const { quantity, serviceId } = req.body;
+
+    if (quantity <= 0) {
       return res.status(400).send({ message: "Invalid quantity" });
     }
-    if (!serviceId){
+    if (!serviceId) {
       return res.status(400).send({ message: "Service not found" });
     }
-  
+
     const item = await Items.create({
       quantity,
       serviceId,
-      cartId: cart.id
+      cartId: cart.id,
     });
     return res.status(201).send({ message: "Item added to cart", item });
-    
-  } catch(e){
-    next(e)
+  } catch (e) {
+    next(e);
   }
-})
+});
+
+router.delete("/", auth, async (req, res, next) => {
+  try {
+    if (req.user.id === null) {
+      return res.status(400).send({ message: "Not logged in!" });
+    }
+    const cart = await Cart.findOne({ where: { userId: req.user.id } });
+    if (cart === null) {
+      return res.status(400).send({ message: "There is no shoppingcart" });
+    }
+    const item = await Items.destroy({ where: { cartId: cart.dataValues.id } });
+    return res.status(201).send({ message: "Card is cleared" });
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = router;
