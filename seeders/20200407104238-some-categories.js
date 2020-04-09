3ef5afb3ebc0bc0a98a6d62b3fb4117a620fa0dd
mariazangelova <mariazangelova@gmail.com>
2020-04-09 08:58:20 +0200

    Add seeds

--- seeders/20200407104238-some-categories.js
+++ seeders/20200407104238-some-categories.js
@@ -1,28 +1,46 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  up:  (queryInterface, Sequelize) => {
-      return queryInterface.bulkInsert('categories', [{
-        name: 'Cleaning',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: 'Exercise',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: 'Special treatment',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }], {});
-
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "categories",
+      [
+        {
+          name: "Cleaning",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Exercise",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "School",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Leisure",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Hostel",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Special treatment",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
-
-      return queryInterface.bulkDelete('categories', null, {});
-
-  }
+    return queryInterface.bulkDelete("categories", null, {});
+  },
 };
